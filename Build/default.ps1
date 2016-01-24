3afc467bb4f398758f087e01dc68ac158590204c
golang69 <george.lang@outlook.com>
2016-01-24 17:16:56 -0500

    initial commit

---
+++ Build/default.ps1
@@ -0,0 +1,57 @@
+﻿properties {
+	$testMessage = 'Executed Test!'  
+	$compileMessage = 'Executed Compile!'  
+	$cleanMessage = 'Executed Clean!'
+
+	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
+	$outputDirectory = "$solutionDirectory\.build"
+	$temporaryOutputDirectory = "$outputDirectory\temp"
+	$buildConfiguration = "Release"
+	$buildPlatform = "Any CPU"
+}
+
+FormatTaskName "`r`n`r`n-------- Executing {0} Task --------"
+
+task default -depends Test
+
+task Init `
+	-description "Initializes the build by removing previous artifacts and creating output directories" `
+	-requiredVariables outputDirectory, temporaryOutputDirectory {
+		Assert ("Debug", "Release" -contains $buildConfiguration) `
+			    "Invaild build configuration '$buildConfiguration'. Valid values are 'Debug' or 'Release'"	
+
+		Assert ("x86", "x64", "Any CPU" -contains $buildPlatform) `
+		        "Invalid build platform '$buildPlatform'. Valid values are 'x86', 'x64' or 'Any CPU'"
+
+	# Remove previous build results
+	if (Test-Path $outputDirectory)
+	{
+		Write-Host "Removing output directory located at $outputDirectory"
+		Remove-Item $outputDirectory -Force -Recurse
+	}
+	Write-Host "Creating output directory located at $outputDirectory"
+	New-Item $outputDirectory -ItemType Directory | Out-Null
+	Write-Host "Creating temporary directory located at $temporaryOutputDirectory"
+	New-Item $temporaryOutputDirectory -ItemType Directory | Out-Null
+}
+
+task Compile `
+    -depends Init `
+	-description "Compile the code" `
+	-requiredVariables solutionFile, buildConfiguration, buildPlatform , temporaryOutputDirectory `
+{ 
+  	Write-Host $compileMessage
+	Exec {
+		msbuild $solutionFile "/p:Configuration=$buildConfiguration;Platform=$buildPlatform;OutDir=$temporaryOutputDirectory"
+	}
+}
+ 
+task Clean -description "Remove temporary files" { 
+  Write-Host $cleanMessage
+}
+
+task Test -depends Compile, Clean -description "Run unit tests" { 
+  Write-Host $testMessage
+}
+
+
